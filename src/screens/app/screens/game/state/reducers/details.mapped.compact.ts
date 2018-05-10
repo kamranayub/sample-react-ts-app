70115608bf06bdb0a9d83f23f3bd523261cf9856
Kamran Ayub <kamran.ayub@gmail.com>
2018-05-09 22:30:59 -0500

    Fix details mapped compact

--- src/screens/app/screens/game/state/reducers/details.mapped.compact.ts
+++ src/screens/app/screens/game/state/reducers/details.mapped.compact.ts
@@ -1,12 +1,22 @@
 import { createReducerActions } from "~/state/reducer";
 import initialState from "../";
-import { LOAD_GAME, LoadGameAction } from "../actions";
+import {
+  LOAD_GAME,
+  LoadGameAction,
+  SAVE_GAME,
+  SaveGameAction
+} from "../actions";
 
-export default createReducerActions(
+type GameAction = LoadGameAction | SaveGameAction;
+
+export default createReducerActions<typeof initialState.details, GameAction>(
   {
     [LOAD_GAME](state, action: LoadGameAction) {
       const { id, game } = action;
       return { ...state, id, game };
+    },
+    [SAVE_GAME](state, action: SaveGameAction) {
+      return { ...state };
     }
   },
   initialState.details
